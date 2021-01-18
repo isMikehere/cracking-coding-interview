50ba22fe5f7572fd296622dd8f9b10672b271ec6
mikelingtao <meng-lingtao@aramark.com>
2021-01-17 22:31:01 -0500

    Add unique char checking

--- src/main/kotlin/com/mlt/cracking/arrays/IsUniqueChar.kt
+++ src/main/kotlin/com/mlt/cracking/arrays/IsUniqueChar.kt
@@ -6,5 +6,31 @@ package com.mlt.cracking.arrays
 cannot use additional data structures?
  */
 class IsUniqueChar {
+    fun version1(strings: String): Boolean {
+        val arr = BooleanArray(128) { false }
+        strings.toCharArray().forEach {
+            if (arr[it.toInt()]) {
+                return false
+            } else {
+                arr[it.toInt()] = true
+            }
+        }
+        return true
+    }
 
+    fun version2(strings: String): Boolean {
+        return strings.toSortedSet().size == strings.length
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            with(IsUniqueChar()) {
+                println(version1("123"))
+                println(version1("122"))
+                println(version2("123"))
+                println(version2("122"))
+            }
+        }
+    }
 }
