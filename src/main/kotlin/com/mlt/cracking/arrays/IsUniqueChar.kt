75e8d7845c8d4c8fbd7208a47fc2ed9bab83eec8
mikelingtao <meng-lingtao@aramark.com>
2021-01-17 19:30:12 -0500

    Init

---
+++ src/main/kotlin/com/mlt/cracking/arrays/IsUniqueChar.kt
@@ -0,0 +1,10 @@
+package com.mlt.cracking.arrays
+
+
+/**
+ *  Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+ */
+class IsUniqueChar {
+
+}
